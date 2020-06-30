0f8cc87f6bfabd709f13ef82ecc42b527e6c3c9b
Lindsay Alexander <linmalex@gmail.com>
2020-06-30 12:31:30 -0700

    initial commit

---
+++ build/precache-manifest.91765cc0032acacef07f496c8a07162e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5178ae7baca52df7d834cc7990ee062",
+    "url": "/bellwether/index.html"
+  },
+  {
+    "revision": "3ddaa966deda7b5b229c",
+    "url": "/bellwether/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a70ffe97c3a161ebdb8",
+    "url": "/bellwether/static/js/2.74c9af1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bellwether/static/js/2.74c9af1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ddaa966deda7b5b229c",
+    "url": "/bellwether/static/js/main.cd026665.chunk.js"
+  },
+  {
+    "revision": "60f3f6e3dcfdc8f3b041",
+    "url": "/bellwether/static/js/runtime-main.eeb8695d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bellwether/static/media/logo.5d5d9eef.svg"
+  }
+]);
